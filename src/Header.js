22ffda30f7b9bd93e2ff71a4df3857cbdd65cdf8
d0mmie <st234@outlook.co.th>
2017-12-06 20:12:22 +0700

    Created Header component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div style={{color: '#336699'}} >
+                Let Me Love You
+            </div>
+        )
+    }
+}
