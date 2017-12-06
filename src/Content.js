192740fff3bf5d6c14067ec2062f6ddbbebcc5c1
d0mmie <st234@outlook.co.th>
2017-12-06 20:18:14 +0700

    Created Content component

---
+++ src/Content.js
@@ -0,0 +1,78 @@
+import React from 'react'
+
+export default class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{`I used to believe`}</p>                
+                <p>{`We were burnin' on the edge of somethin' beautiful`}</p>
+                <p>{`Somethin' beautiful`}</p>
+                <p>{`Sellin' a dream`}</p>
+                <p>{`Smoke and mirrors keep us waiting on a miracle`}</p>
+                <p>{`On a miracle`}</p>
+                <br/>
+                <p>{`Say, go through the darkest of days`}</p>
+                <p>{`Heaven's a heartbreak away`}</p>
+                <p>{`Never let you go, never let me down`}</p>
+                <p>{`Oh, it's been a hell of a ride`}</p>
+                <p>{`Driving the edge of a knife`}</p>
+                <p>{`Never let you go, never let me down`}</p>
+                <br/>
+                <p>{`Don't you give up, nah-nah-nah`}</p>
+                <p>{`I won't give up, nah-nah-nah`}</p>
+                <p>{`Let me love you`}</p>
+                <p>{`Let me love you`}</p>
+                <p>{`Don't you give up, nah-nah-nah`}</p>
+                <p>{`I won't give up, nah-nah-nah`}</p>
+                <p>{`Let me love you`}</p>
+                <p>{`Let me love you`}</p>
+                <p>{`Oh, baby, baby`}</p>
+                <br/>
+                <p>{`Don't fall asleep`}</p>
+                <p>{`At the wheel, we've got a million miles ahead of us`}</p>
+                <p>{`Miles ahead of us`}</p>
+                <p>{`All that we need`}</p>
+                <p>{`Is a rude awakening to know we're good enough`}</p>
+                <p>{`Know we're good enough`}</p>
+                <br/>
+                <p>{`Say go through the darkest of days`}</p>
+                <p>{`Heaven's a heartbreak away`}</p>
+                <p>{`Never let you go, never let me down`}</p>
+                <p>{`Oh, it's been a hell of a ride`}</p>
+                <p>{`Driving the edge of a knife`}</p>
+                <p>{`Never let you go, never let me down`}</p>
+                <br/>
+                <p>{`Don't you give up, nah-nah-nah`}</p>
+                <p>{`I won't give up, nah-nah-nah`}</p>
+                <p>{`Let me love you`}</p>
+                <p>{`Let me love you`}</p>
+                <p>{`Don't you give up, nah-nah-nah`}</p>
+                <p>{`I won't give up, nah-nah-nah`}</p>
+                <p>{`Let me love you`}</p>
+                <p>{`Let me love you`}</p>
+                <p>{`Oh, baby, baby`}</p>
+                <br/>
+                <p>{`Never let you go, never let you go go`}</p>
+                <p>{`Never let you go, never let you go go`}</p>
+                <p>{`Never let you go, whoa`}</p>
+                <p>{`No, never let you go, whoa`}</p>
+                <br/>
+                <p>{`Never let you go, never let you go go`}</p>
+                <p>{`Never let you go, never let you go go`}</p>
+                <p>{`Oh, no, no, no`}</p>
+                <p>{`No, never let you go`}</p>
+                <p>{`Yeah, yeah`}</p>
+                <p>{`No, never let you go`}</p>
+                <br/>
+                <p>{`Don't you give up, nah-nah-nah`}</p>
+                <p>{`I won't give up, nah-nah-nah`}</p>
+                <p>{`Let me love you`}</p>
+                <p>{`Let me love you`}</p>
+                <p>{`Don't you give up, nah-nah-nah`}</p>
+                <p>{`I won't give up, nah-nah-nah`}</p>
+                <p>{`Let me love you`}</p>
+                <p>{`Let me love you`}</p>
+            </div>
+        )
+    }
+}
